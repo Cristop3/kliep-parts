e4fa54d052cf0b7a9bb63136440cb74dc221b828
Caristop3 <10445242@qq.com>
2023-12-15 16:12:48 +0800

    update

---
+++ assets/components_index.md.REEoK2Hh.lean.js
@@ -0,0 +1,16 @@
+import{f as d,w as F,s}from"./chunks/index.MjdE9xpz.js";import{d as x,D as a,o as c,b,w as l,I as i,c as m,E as D,F as T,t as n,k as e,a as o}from"./chunks/framework.0_9lIZ6q.js";const C={style:{"text-align":"right"}},w=x({__name:"index",setup(p){const r=[{title:"【发布】0.1.2版本",author:"Caristop3",gitTime:"Wed Dec 15 2023 10:03:21",content:`
+      
+    `,timestamp:"2023-12-15",color:"#0000FF",icon:d},{title:"【修复】kd-filter-table模板模式下获取不到scope",author:"Caristop3",gitTime:"Wed Dec 15 2023 10:03:21",content:`
+      1. 重构关于props和template模式逻辑
+      2. 完全支持嵌套表格字段
+      3. 增加cellRender h函数渲染
+    `,timestamp:"2023-12-15",color:"#FF0000",icon:F},{title:"【发布】0.1.1版本",author:"Caristop3",gitTime:"Wed Dec 13 2023 10:03:21",content:`
+      1. 增加外层npmrc配置私域
+    `,timestamp:"2023-12-12",color:"#0000FF",icon:d},{title:"【增加】kd-upload、kd-el-col组件",author:"Caristop3",gitTime:"Tue Dec 12 2023 17:15:24",content:`
+      1. 增加kd-upload
+      2. 增加kd-el-col
+    `,timestamp:"2023-12-12",color:"#00FF00",icon:s},{title:"【增加】kd-page、kd-filter-table、kd-page组件",author:"Caristop3",gitTime:"Mon Dec 04 2023 11:36:58",content:`
+      1. 增加kd-page
+      2. 增加kd-filter-table
+      3. 增加kd-page
+    `,timestamp:"2023-12-04",color:"#00FF00",icon:s},{title:"【初始化】公共库UIToolsDocs",author:"Caristop3",gitTime:"Fri Dec 01 2023 15:48:49",content:"采用pnpm包管理方式，启动kliep-parts工程，子工程包含docs、package、examples等等",timestamp:"2023-12-01",color:"#00FF00",icon:s}];return(_,V)=>{const u=a("el-card"),h=a("el-timeline-item"),f=a("el-timeline"),g=a("el-scrollbar");return c(),b(g,{height:520},{default:l(()=>[i(f,null,{default:l(()=>[(c(),m(T,null,D(r,(t,k)=>i(h,{key:k,icon:t.icon,color:t.color,size:"large",timestamp:t.timestamp,placement:"top",center:""},{default:l(()=>[i(u,{header:"",shadow:"hover"},{default:l(()=>[e("h4",null,n(t.title),1),o(" "+n(t.content)+" ",1),e("p",C,n(t.author)+" 提交于 "+n(t.gitTime),1)]),_:2},1024)]),_:2},1032,["icon","color","timestamp"])),64))]),_:1})]),_:1})}}}),B=e("h4",{id:"目前版本-0-1-2",tabindex:"-1"},[o("目前版本 0.1.2 "),e("a",{class:"header-anchor",href:"#目前版本-0-1-2","aria-label":'Permalink to "目前版本 0.1.2"'},"​")],-1),N=e("ul",null,[e("li",null,[e("code",null,"绿星星"),o("：新增功能 "),e("code",null,"红警告"),o("：修改bug "),e("code",null,"蓝旗子"),o("：版本")])],-1),I=JSON.parse('{"title":"","description":"","frontmatter":{"aside":false,"docFooter":false},"headers":[],"relativePath":"components/index.md","filePath":"components/index.md","lastUpdated":1702627754000}'),P={name:"components/index.md"},O=Object.assign(P,{setup(p){return(r,_)=>(c(),m("div",null,[B,N,i(w)]))}});export{I as __pageData,O as default};
