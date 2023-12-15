0424c295f042638e4bdaba0ecfe89f1088ffcf37
Caristop3 <10445242@qq.com>
2023-12-15 17:40:04 +0800

    update

---
+++ assets/components_index.md.vHdPhVgy.lean.js
@@ -0,0 +1,21 @@
+import{f as c,w as d,s as r}from"./chunks/index.MjdE9xpz.js";import{d as x,D as a,o as s,b as D,w as l,I as i,c as p,E as T,F as b,t as n,k as e,a as o}from"./chunks/framework.0_9lIZ6q.js";const C={style:{"text-align":"right"}},w=x({__name:"index",setup(_){const m=[{title:"【发布】0.1.3版本",author:"Caristop3",gitTime:"Wed Dec 15 2023 17:33:21",content:`
+      
+    `,timestamp:"2023-12-15",color:"#0000FF",icon:c},{title:"【修复】kd-filter-table",author:"Caristop3",gitTime:"Wed Dec 15 2023 17:30:21",content:`
+      1. 修复二次赋值filterList后 并未更新视图
+      2. 【fix】更新最新filterList 老版本是computed v-for 现在是变量
+    `,timestamp:"2023-12-15",color:"#FF0000",icon:d},{title:"【发布】0.1.2版本",author:"Caristop3",gitTime:"Wed Dec 15 2023 10:03:21",content:`
+      
+    `,timestamp:"2023-12-15",color:"#0000FF",icon:c},{title:"【修复】kd-filter-table模板模式下获取不到scope",author:"Caristop3",gitTime:"Wed Dec 15 2023 10:03:21",content:`
+      1. 重构关于props和template模式逻辑
+      2. 完全支持嵌套表格字段
+      3. 增加cellRender h函数渲染
+    `,timestamp:"2023-12-15",color:"#FF0000",icon:d},{title:"【发布】0.1.1版本",author:"Caristop3",gitTime:"Wed Dec 13 2023 10:03:21",content:`
+      1. 增加外层npmrc配置私域
+    `,timestamp:"2023-12-12",color:"#0000FF",icon:c},{title:"【增加】kd-upload、kd-el-col组件",author:"Caristop3",gitTime:"Tue Dec 12 2023 17:15:24",content:`
+      1. 增加kd-upload
+      2. 增加kd-el-col
+    `,timestamp:"2023-12-12",color:"#00FF00",icon:r},{title:"【增加】kd-page、kd-filter-table、kd-page组件",author:"Caristop3",gitTime:"Mon Dec 04 2023 11:36:58",content:`
+      1. 增加kd-page
+      2. 增加kd-filter-table
+      3. 增加kd-page
+    `,timestamp:"2023-12-04",color:"#00FF00",icon:r},{title:"【初始化】公共库UIToolsDocs",author:"Caristop3",gitTime:"Fri Dec 01 2023 15:48:49",content:"采用pnpm包管理方式，启动kliep-parts工程，子工程包含docs、package、examples等等",timestamp:"2023-12-01",color:"#00FF00",icon:r}];return(u,L)=>{const f=a("el-card"),h=a("el-timeline-item"),g=a("el-timeline"),k=a("el-scrollbar");return s(),D(k,{height:520},{default:l(()=>[i(g,null,{default:l(()=>[(s(),p(b,null,T(m,(t,F)=>i(h,{key:F,icon:t.icon,color:t.color,size:"large",timestamp:t.timestamp,placement:"top",center:""},{default:l(()=>[i(f,{header:"",shadow:"hover"},{default:l(()=>[e("h4",null,n(t.title),1),o(" "+n(t.content)+" ",1),e("p",C,n(t.author)+" 提交于 "+n(t.gitTime),1)]),_:2},1024)]),_:2},1032,["icon","color","timestamp"])),64))]),_:1})]),_:1})}}}),W=e("h4",{id:"目前版本-0-1-2",tabindex:"-1"},[o("目前版本 0.1.2 "),e("a",{class:"header-anchor",href:"#目前版本-0-1-2","aria-label":'Permalink to "目前版本 0.1.2"'},"​")],-1),B=e("ul",null,[e("li",null,[e("code",null,"绿星星"),o("：新增功能 "),e("code",null,"红警告"),o("：修改bug "),e("code",null,"蓝旗子"),o("：版本")])],-1),E=JSON.parse('{"title":"","description":"","frontmatter":{"aside":false,"docFooter":false},"headers":[],"relativePath":"components/index.md","filePath":"components/index.md","lastUpdated":1702627754000}'),N={name:"components/index.md"},I=Object.assign(N,{setup(_){return(m,u)=>(s(),p("div",null,[W,B,i(w)]))}});export{E as __pageData,I as default};
