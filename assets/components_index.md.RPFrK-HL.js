9ec3325495ea23967d39ed636b59b7cc8bd3f86a
Caristop3 <10445242@qq.com>
2023-12-19 14:11:14 +0800

    update

---
+++ assets/components_index.md.RPFrK-HL.js
@@ -0,0 +1,26 @@
+import{f as o,w as a,s}from"./chunks/index.MjdE9xpz.js";import{d as g,D as n,o as p,b as C,w as i,I as r,c as d,E as x,F as w,t as c,k as t,a as l}from"./chunks/framework.0_9lIZ6q.js";const D={style:{"text-align":"right"}},B=g({__name:"index",setup(_){const m=[{title:"【发布】0.1.6版本",author:"Caristop3",content:"",timestamp:"2023-12-19",color:"#0000FF",icon:o},{title:"【修复】kd-filter-table",author:"Caristop3",content:`
+      1. 【fix】解决非非驼峰的showOverflowTooltip和header-align等处理
+    `,timestamp:"2023-12-19",color:"#FF0000",icon:a},{title:"【发布】0.1.5版本",author:"Caristop3",content:"",timestamp:"2023-12-19",color:"#0000FF",icon:o},{title:"【修复】kd-filter-table & kliep-tools",author:"Caristop3",content:`
+      1. 【feat】增加table内部主动根据label字数设置min-width，不再繁琐的从外面设置，但不支持嵌套层级
+      2. 【feat】增加常见的枚举值 比如： 是&否 已启用&已停用 停用&启用 
+      3. 【feat】增加方法：funcDeepAssignObject
+    `,timestamp:"2023-12-19",color:"#FF0000",icon:a},{title:"【发布】0.1.4版本",author:"Caristop3",content:"",timestamp:"2023-12-18",color:"#0000FF",icon:o},{title:"【修复】kd-filter-table",author:"Caristop3",content:`
+      1. 【fix】表格字段过长 工具bar超出显示范围 增加滚动
+      2. 【feat】增加全局配置table属性
+    `,timestamp:"2023-12-18",color:"#FF0000",icon:a},{title:"【发布】0.1.3版本",author:"Caristop3",content:"",timestamp:"2023-12-15",color:"#0000FF",icon:o},{title:"【修复】kd-filter-table",author:"Caristop3",content:`
+      1. 修复二次赋值filterList后 并未更新视图
+      2. 【fix】更新最新filterList 老版本是computed v-for 现在是变量
+    `,timestamp:"2023-12-15",color:"#FF0000",icon:a},{title:"【发布】0.1.2版本",author:"Caristop3",content:"",timestamp:"2023-12-15",color:"#0000FF",icon:o},{title:"【修复】kd-filter-table模板模式下获取不到scope",author:"Caristop3",content:`
+      1. 重构关于props和template模式逻辑
+      2. 完全支持嵌套表格字段
+      3. 增加cellRender h函数渲染
+    `,timestamp:"2023-12-15",color:"#FF0000",icon:a},{title:"【发布】0.1.1版本",author:"Caristop3",content:`
+      1. 增加外层npmrc配置私域
+    `,timestamp:"2023-12-12",color:"#0000FF",icon:o},{title:"【增加】kd-upload、kd-el-col组件",author:"Caristop3",content:`
+      1. 增加kd-upload
+      2. 增加kd-el-col
+    `,timestamp:"2023-12-12",color:"#00FF00",icon:s},{title:"【增加】kd-page、kd-filter-table、kd-page组件",author:"Caristop3",content:`
+      1. 增加kd-page
+      2. 增加kd-filter-table
+      3. 增加kd-page
+    `,timestamp:"2023-12-04",color:"#00FF00",icon:s},{title:"【初始化】公共库UIToolsDocs",author:"Caristop3",content:"采用pnpm包管理方式，启动kliep-parts工程，子工程包含docs、package、examples等等",timestamp:"2023-12-01",color:"#00FF00",icon:s}];return(u,P)=>{const f=n("el-card"),h=n("el-timeline-item"),F=n("el-timeline"),k=n("el-scrollbar");return p(),C(k,{"max-height":480},{default:i(()=>[r(F,null,{default:i(()=>[(p(),d(w,null,x(m,(e,b)=>r(h,{key:b,icon:e.icon,color:e.color,size:"large",timestamp:e.timestamp,placement:"top",center:""},{default:i(()=>[r(f,{header:"",shadow:"hover"},{default:i(()=>[t("h4",null,c(e.title),1),l(" "+c(e.content)+" ",1),t("p",D,c(e.author)+" 提交",1)]),_:2},1024)]),_:2},1032,["icon","color","timestamp"])),64))]),_:1})]),_:1})}}}),N=t("h4",{id:"目前版本-0-1-6",tabindex:"-1"},[l("目前版本 0.1.6 "),t("a",{class:"header-anchor",href:"#目前版本-0-1-6","aria-label":'Permalink to "目前版本 0.1.6"'},"​")],-1),O=t("ul",null,[t("li",null,[t("code",null,"绿星星"),l("：新增功能 "),t("code",null,"红警告"),l("：修改bug "),t("code",null,"蓝旗子"),l("：版本")])],-1),j=JSON.parse('{"title":"","description":"","frontmatter":{"aside":false},"headers":[],"relativePath":"components/index.md","filePath":"components/index.md","lastUpdated":1702955446000}'),L={name:"components/index.md"},v=Object.assign(L,{setup(_){return(m,u)=>(p(),d("div",null,[N,O,r(B)]))}});export{j as __pageData,v as default};
